72cd11bdd42b4ba5c98c3e31a8399a0b7577a545
Felix Jordan <felix.jordan@buddy-app.de>
2021-11-28 23:16:58 +0100

    install PrimeVue, together with PrimeIcons and PrimeFlex

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css' 
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(PrimeVue);
+
+app.mount('#app')
